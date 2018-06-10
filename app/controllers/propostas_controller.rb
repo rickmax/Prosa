475f9601e0f0ccaa57fe686cc8a0a063ab5d1571
Rick <rickmaxg3@hotmail.com>
2018-06-10 09:48:27 -0300

    configurando rotas

--- app/controllers/propostas_controller.rb
+++ app/controllers/propostas_controller.rb
@@ -1,2 +1,56 @@
 class PropostasController < ApplicationController
+    skip_before_filter :verify_authenticity_token  
+    before_action :set_proposta, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @propostas = Propostum.all
+    
+    render json: @propostas
+  end
+
+  def show
+    @proposta = Propostum.find(params[:id])
+    render json: @proposta
+  end
+
+  def new
+    @proposta = Propostum.new
+  end
+
+  def edit
+    @proposta = Propostum.find(params[:id])
+    render json: @proposta
+  end
+
+  def create
+    @proposta = Propostum.new(proposta_params)
+
+      if @proposta.save
+        render json: @propostas
+      else
+        render json: @propostas.errors
+      end
+  end
+
+  def update
+    if @proposta.update(proposta_params)
+      render json: @proposta
+    else
+      render json: @proposta.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @proposta.destroy
+    render json: @proposta
+  end
+
+  private
+    def set_proposta
+      @proposta = Propostum.find(params[:id])
+    end
+
+    def proposta_params
+      params.require(:proposta).permit(:nome, :descricao, :valor, :inicio, :fim, :estado, :cidade)
+    end
 end
